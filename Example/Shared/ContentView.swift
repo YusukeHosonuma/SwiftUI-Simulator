4109b2f5dd158bc92d2938ccaec2a8fae0720053
Yusuke Hosonuma <tobi462@gmail.com>
2022-05-06 12:52:41 +0900

    refactor: extract file

--- Example/Shared/ContentView.swift
+++ Example/Shared/ContentView.swift
@@ -104,16 +104,6 @@ struct ContentView: View {
     }
 }
 
-extension View {
-    func simulatableSheet<Content: View>(isPresented: Binding<Bool>, @ViewBuilder content: @escaping () -> Content) -> some View {
-        #if DEBUG
-        simulatedSheet(isPresented: isPresented, content: content)
-        #else
-        sheet(isPresented: isPresented, content: content)
-        #endif
-    }
-}
-
 struct SampleSheetView: View {
     //
     // ☑️ Standard API (iOS 14+)
