8cc23c701cff437046a5644620c27ecef7cc4775
Yusuke Hosonuma <tobi462@gmail.com>
2022-05-06 17:52:12 +0900

    clean: use external package

--- Package.swift
+++ Package.swift
@@ -14,15 +14,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/sindresorhus/Defaults.git", from: "6.2.0"),
         .package(url: "https://github.com/YusukeHosonuma/SwiftPrettyPrint.git", from: "1.2.0"),
+        .package(url: "https://github.com/YusukeHosonuma/UserDefaultsBrowser.git", branch: "main"),
     ],
     targets: [
         .target(name: "SwiftUISimulator", dependencies: [
             "Defaults",
             "SwiftPrettyPrint", // TODO: can remove?
             "UserDefaultsBrowser",
-        ]),
-        .target(name: "UserDefaultsBrowser", dependencies: [
-            "SwiftPrettyPrint",
         ]),
         .testTarget(name: "SwiftUISimulatorTests", dependencies: ["SwiftUISimulator"]),
     ]
